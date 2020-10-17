fb57027a3bdadc7229c1309262a8b79fe0b7ca12
XeverDead <xever.dead.115@gmail.com>
2020-10-17 13:17:44 +0300

    First home task I guess

---
+++ FileHosting/FileHosting/AuthenticationMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileHosting
+{
+    public class AuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string _validToken;
+
+        public AuthenticationMiddleware(RequestDelegate next, string validToken)
+        {
+            _next = next;
+            _validToken = validToken;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Query["Token"].Equals(_validToken))
+            {
+                await _next.Invoke(context);
+            }
+            else
+            {
+                context.Response.StatusCode = 403;
+            }
+        }
+    }
+}
---
+++ FileHosting/FileHosting/Startup.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FileHosting
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync(env.ContentRootPath);
+            //    });
+            //});
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.UseMiddleware<AuthenticationMiddleware>("secret");
+            app.UseStaticFiles();
+        }
+    }
+}
