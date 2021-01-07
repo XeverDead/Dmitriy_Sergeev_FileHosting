059e5b5cdaf7e65c28a4c63230f4b0f7277508c0
XeverDead <xever.dead.115@gmail.com>
2021-01-07 17:21:18 +0300

    Better interface and some fixes

--- FileHosting/FileHosting/Controllers/HostingController.cs
+++ FileHosting/FileHosting/Controllers/HostingController.cs
@@ -24,7 +24,6 @@ using Web.Enums;
 
 namespace Web.Controllers
 {
-    [Authorize]
     public class HostingController : Controller
     {
         private readonly IHostingCore _hostingCore;
@@ -39,13 +38,23 @@ namespace Web.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View(_hostingCore.GetAllUsers());
+            return View();
         }
 
         [HttpGet]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
         public IActionResult UserPage(long userId)
-        {         
-            var user = _hostingCore.GetUserById(userId);
+        {
+            User user;
+
+            if (userId == -1)
+            {
+                user = _hostingCore.GetUserByEmail(User.Identity.Name);
+            }
+            else
+            {
+                user = _hostingCore.GetUserById(userId);
+            }
 
             if (user == null)
             {
@@ -55,7 +64,7 @@ namespace Web.Controllers
             var userFiles = new UserFilesModel
             {
                 User = user,
-                Files = _hostingCore.GetUserFiles(userId)
+                Files = _hostingCore.GetUserFiles(user.Id)
             };
 
             return View(userFiles);
@@ -81,6 +90,7 @@ namespace Web.Controllers
         }
 
         [HttpGet]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
         public IActionResult UsersSearch(string login)
         {
             if (string.IsNullOrWhiteSpace(login))
@@ -120,88 +130,152 @@ namespace Web.Controllers
         }
 
         [HttpGet]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
         public IActionResult ChangeUserInfo(long userId)
         {
             var user = _hostingCore.GetUserById(userId);
 
-            var userInfo = new UserInfoModel
+            if (User.Identity.Name == user.Email ||
+                User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Admin.ToString()) ||
+                User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Editor.ToString()))
             {
-                Id = user.Id,
-                Login = user.Login,
-                Role = user.Role
-            };
+                var userInfo = new UserInfoModel
+                {
+                    Id = user.Id,
+                    Login = user.Login,
+                    Role = user.Role
+                };
 
-            return View(userInfo);
-        }
-
-        [HttpPost]
-        public IActionResult ChangeUserInfo(UserInfoModel userInfo)
-        {
-            var user = _hostingCore.GetUserById(userInfo.Id);
-
-            user.Login = userInfo.Login;
-            user.Role = userInfo.Role;
-
-            _hostingCore.UpdateUser(user.Id, user);
+                return View(userInfo);
+            }
 
             return RedirectToAction("UserPage", new { userId = user.Id });
         }
 
+        [HttpPost]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
+        public IActionResult ChangeUserInfo(UserInfoModel userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _hostingCore.GetUserById(userInfo.Id);
+
+                if (User.Identity.Name == user.Email ||
+                    User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Admin.ToString()) ||
+                    User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Editor.ToString()))
+                {
+                    user.Login = userInfo.Login;
+                    user.Role = userInfo.Role;
+
+                    _hostingCore.UpdateUser(user.Id, user);
+                }
+
+                return RedirectToAction("UserPage", new { userId = user.Id });
+            }
+
+            return View(userInfo);
+        }
+
         [HttpGet]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
         public IActionResult ChangeFileInfo(long fileId)
         {
             var file = _hostingCore.GetFileById(fileId);
+            var user = _hostingCore.GetUserById(file.AuthorId);
 
-            var fileInfo = new FileInfoModel
+            if (User.Identity.Name == user.Email ||
+                User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Admin.ToString()) ||
+                User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Editor.ToString()))
             {
-                Id = file.Id,
-                Name = file.Name,
-                Category = file.Category,
-                Description = file.Description
-            };
+                var fileInfo = new FileInfoModel
+                {
+                    Id = file.Id,
+                    Name = file.Name,
+                    Category = file.Category,
+                    Description = file.Description
+                };
 
-            return View(fileInfo);
-        }
-
-        [HttpPost]
-        public IActionResult ChangeFileInfo(FileInfoModel fileInfo)
-        {
-            var file = _hostingCore.GetFileById(fileInfo.Id);
-
-            file.Name = fileInfo.Name;
-            file.Category = fileInfo.Category;
-            file.Description = fileInfo.Description;
-
-            _hostingCore.UpdateFile(file.Id, file);
+                return View(fileInfo);
+            }
 
             return RedirectToAction("FilePage", new { fileId = file.Id });
         }
 
+        [HttpPost]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
+        public IActionResult ChangeFileInfo(FileInfoModel fileInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                var file = _hostingCore.GetFileById(fileInfo.Id);
+                var user = _hostingCore.GetUserById(file.AuthorId);
+
+                if (User.Identity.Name == user.Email ||
+                    User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Admin.ToString()) ||
+                    User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Editor.ToString()))
+                {
+                    var userFiles = _hostingCore.GetUserFiles(file.AuthorId);
+
+                    var selectedFiles = from userFile in userFiles
+                                        where userFile.Name == fileInfo.Name
+                                        select userFile;
+
+                    if (selectedFiles?.Count() > 0)
+                    {
+                        ModelState.AddModelError("", "You already have a file with this name");
+
+                        return View(fileInfo);
+                    }
+
+                    file.Name = fileInfo.Name;
+                    file.Category = fileInfo.Category;
+                    file.Description = fileInfo.Description;
+
+                    _hostingCore.UpdateFile(file.Id, file);
+                }
+
+                return RedirectToAction("FilePage", new { fileId = file.Id });
+            }
+
+            return View(fileInfo);
+        }
+
         [HttpGet]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
         public IActionResult DeleteFile(long fileId)
         {
             var file = _hostingCore.GetFileById(fileId);
+            var user = _hostingCore.GetUserById(file.AuthorId);
 
-            System.IO.File.Delete(Path.Combine(_appEnvironment.WebRootPath, file.Link));
-            _hostingCore.DeleteFile(fileId);
+            if (User.Identity.Name == user.Email || 
+                User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Admin.ToString()))
+            {
+                System.IO.File.Delete(Path.Combine(_appEnvironment.WebRootPath, file.Link));
+                _hostingCore.DeleteFile(fileId);
+            }
 
             return RedirectToAction("UserPage", new { userId = file.AuthorId});
         }
 
         [HttpGet]
+        [RoleAuthorize(Roles.User, Roles.Editor, Roles.Admin)]
         public IActionResult DeleteUser(long userId)
         {
-            var user = _hostingCore.GetUserById(userId);
-
-            var files = _hostingCore.GetUserFiles(userId);
-
-            foreach (var file in files)
+            if (User.HasClaim(ClaimsIdentity.DefaultRoleClaimType, Roles.Admin.ToString()))
             {
-                System.IO.File.Delete(Path.Combine(_appEnvironment.WebRootPath, file.Link));
-                _hostingCore.DeleteFile(file.Id);
-            }
+                var user = _hostingCore.GetUserById(userId);
 
-            _hostingCore.DeleteUser(user.Id);
+                var files = _hostingCore.GetUserFiles(userId);
+
+                foreach (var file in files)
+                {
+                    System.IO.File.Delete(Path.Combine(_appEnvironment.WebRootPath, file.Link));
+                    _hostingCore.DeleteFile(file.Id);
+                }
+
+                _hostingCore.DeleteUser(user.Id);
+
+            }
 
             return RedirectToAction("Index");
         }
